fd89e4a30773981ab00825bf220eb0a03325cf56
Quantum Homes <toluwalope.david@gmail.com>
2026-06-28 11:26:50 +0100

    Added navy blue color

--- src/sections/services/ServiceSection.tsx
+++ src/sections/services/ServiceSection.tsx
@@ -35,7 +35,7 @@ export default function SeviceSection(props: {
                 maxHeight: isOpen ? "70rem" : "14rem",
                 transitionDuration: "0.5s",
             }}
-            className="w-full cursor-pointer hover:bg-zinc-50 border-t border-zinc-200 py-14 transition-all ease-in-out duration-300"
+            className="w-full cursor-pointer hover:bg-black/5 border-t border-zinc-900/10 py-14 transition-all ease-in-out duration-300"
         >
             <div className={`flex lg:flex-row flex-col items-center justify-between gap-7 xl:max-w-[1400px] lg:max-w-[1000px] m-auto ${isOpen ? 'max-h-[40rem]' : 'max-h-[6rem]'} transition-all ease-in-out duration-300 h-full px-5`}>
                 <div className={`${isOpen ? "sm:flex flex" : "sm:flex hidden" } xl:justify-end lg:justify-start items-center w-full`}>
